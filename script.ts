4bc589651cc1c71eff3ab5d71794e80e5b2190b5
Sicelo Sitsha <70627871+sicelo17@users.noreply.github.com>
2024-07-11 17:06:18 +0200

    basic add client to db using prisma

--- script.ts
+++ script.ts
@@ -2,13 +2,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    // prisma queries in here
+    const users = await prisma.user.findMany()
+    console.log(users)
 }
 
 main() 
     .catch((e) => {
-        console.error(e);
+        console.error(e)
     })
     .finally(async () => {
-        await prisma.$disconnect();
-    })
+        await prisma.$disconnect()
+    });
